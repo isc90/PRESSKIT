9e6e84da339b17ddcf3541eb0daa03a9a2c86a7f
captainSolo <captainsolo23@gmail.com>
2023-10-04 19:27:25 -0600

    ignore

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,8 +3,6 @@ const router = express.Router()
 const { registerUser, loginUser, getUserData, editUser, getProfile, getUserVcf } = require('../controllers/userControllers')
 const { auth } = require('../middleware/authMiddleware')
 const { upload } = require('../middleware/uploadMiddleware')
-const { uploadImage } = require('../utils/cloudinary')
-
 
 // public
 router.post('/register', registerUser)
@@ -16,25 +14,21 @@ router.get('/vcf/:nickname', getUserVcf)
 router.get('/me', auth, getUserData)
 router.post('/editMyInfo/:id', auth, upload.single('photo'), editUser)
 
-router.post('/uploadProfilePicture', auth, upload.single('photo'), async (req, res, next) => {
-  const file = req.file
-  if (!file) {
-    const error = new Error('Please upload a file')
-    error.httpStatusCode = 400
-    return next(error)
+router.post('/uploadProfilePicture', auth, upload.single('photo'), async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({ error: 'No se proporcionó ningún archivo' })
+    }
+
+    const user = req.user
+    user.photo = req.file.path
+    await user.save()
+
+    res.json({ photo: user.photo })
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ error: 'Error al cargar y guardar la imagen' })
   }
-  res.send(file)
-  uploadImage(file.path)
 })
 
 module.exports = router
-/**
-
-try {
-    await uploadProfilePicture(req, res, user)
-  } catch (error) {
-    console.error(error)
-    res.status(500).json({ message: 'Error al cargar la foto de perfil' })
-  }
-
- */
